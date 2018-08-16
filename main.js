14295a37bf51fe70bd8b66e6a11b9bd704e689b5
Tolulope Adeleye <adeleyetolu9250@gmail.com>
2018-08-16 07:27:25 +0100

    chnages made to clear game

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let player1 = 1;
 let playerturn = 1;
 let gridarray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+let gridarray1 = new Array('-', '-', '-', '-', '-', '-', '-', '-', '-');
 
 /* function things(){
     setPlayer();
@@ -100,6 +101,7 @@ function win(buttonId){
         gridarray[2]==gridarray[4] && gridarray[4]==gridarray[6] && gridarray[2] != 0
     ){
         console.log ('tada');
+        alert(`${playerturn} won`);
         return true;
     }
     else {
@@ -109,24 +111,55 @@ function win(buttonId){
         }
         else {
             console.log('tada');
+            alert('no more moves');
             return true;
         }
     }
 }
 
-function endgame(){
+function endgame(buttonId){
     if (win() == true){
-        cleargame();
+        
+        cleargame(buttonId);
         console.log('win')
+        // squareClicked(buttonId);
+
     }
     else {
         changePlayer();
     }
 }
 
-function cleargame(){
+function cleargame(buttonId){
+    
     for (let i=0; i<9; i++){
-        gridarray[i] = 0;
-        gridarray[i].innerHTML = '-';
+        let x = document.getElementById("" + i);
     }
+    count = document.all('x').length;
+    // console.log(count);
+
+    count1 = document.all('o').length;
+    // console.log(count1);
+    let result = document.querySelectorAll('#x');
+    for (let i=(count-1); i >= 0; i--){
+        // let result = document.querySelectorAll('#x');
+        // console.log(result[i]);
+        result[i].innerText = '-'
+        result[i].id = `${i+8}`;
+    }
+
+    for (let i=(count1-1); i >= 0; i--){
+        let result = document.querySelectorAll('#o');
+        // console.log(result[i]);
+        console.log(i);
+        result[i].innerText = '-';
+        result[i].id = `${i+8}`;
+        console.log(result[i].id);
+        console.log(result);
+        gridarray = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
+    
+    console.log(gridarray);
+    let y = document.getElementsByClassName('squareButton');
+    console.log(y[0].id);
 }
